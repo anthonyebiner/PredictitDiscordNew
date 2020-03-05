7e23a6c4625f322f0880edc9684ff46bb72671e4
Anthony Ebiner <anthonyebiner@berkeley.edu>
2020-03-05 00:39:32 -0800

    add more to classes

--- api.py
+++ api.py
@@ -1,5 +1,10 @@
 import requests
 import auths
+from utils import optShares
+import math
+import re
+from fuzzywuzzy import fuzz
+
 
 class Offer:
     def __init__(self, offer_dict):
@@ -19,24 +24,45 @@ class Contract:
     @property
     def yes_offers(self):
         if not self._yes:
-            yield None
-        for yes in self._yes:
-            yield Offer(yes)
+            empty_offer = {
+                "costPerShareNo": 1,
+                "costPerShareYes": 0,
+                "pricePerShare": 0,
+                "quantity": 0,
+                "tradeType": 0
+            }
+            yield Offer(empty_offer)
+        else:
+            for yes in self._yes:
+                yield Offer(yes)
+
+    @property
+    def best_yes(self):
+        return list(self.yes_offers)[0]
 
     @property
     def no_offers(self):
         if not self._no:
-            return None
-        for no in self._no:
-            yield Offer(no)
+            empty_offer = {
+                "costPerShareNo": 0,
+                "costPerShareYes": 1,
+                "pricePerShare": 0,
+                "quantity": 0,
+                "tradeType": 1
+            }
+            yield Offer(empty_offer)
+        else:
+            for no in self._no:
+                yield Offer(no)
+
+    @property
+    def best_no(self):
+        return list(self.no_offers)[0]
 
     def buy_no(self, bin, num_shares, price):
         pass
 
     def buy_yes(self, bin, num_shares, price):
-        pass
-
-    def buy_spread(self, spread):
         pass
 
 
@@ -55,8 +81,72 @@ class Market:
         for contract in self._contracts:
             yield Contract(self._orderbook[str(contract['id'])])
 
+    def no_prices(self):
+        prices = []
+        for contract in self.contracts:
+            if contract.best_no.pricePerShare != 0:
+                prices += [contract.best_no.pricePerShare]
+        return prices
 
-class API:
+    def yes_prices(self):
+        prices = []
+        for contract in self.contracts:
+            if contract.best_yes.pricePerShare != 0:
+                prices += [contract.best_yes.pricePerShare]
+        return prices
+
+    def sum_return(self):
+        total = 0
+        for price in self.no_prices():
+            total += 1 - price
+        return round(total, 3)
+
+    def _calc_risk(self, spread):
+        prices = [contract.best_no.pricePerShare for contract in list(self.contracts)]
+        share_value = [spread[i] * prices[i] for i in range(len(prices))]
+        if_no = [round((spread[i] - share_value[i]) - 0.1 * (spread[i] - share_value[i]), 3) for i in
+                 range(len(prices))]
+        if_yes = [-(share_value[i]) for i in range(len(prices))]
+        risk = [round(if_yes[i] + sum(if_no[0:i]) + sum(if_no[i + 1:]), 3) for i in range(len(prices))]
+        return risk
+
+    def _potential_profit(self, spread):
+        return min(self._calc_risk(spread))
+
+    def _best_amount(self, max_shares):
+        contracts = list(self.contracts)
+        costliest = max(self.no_prices())
+        share_multiplier = (max_shares / costliest) / optShares(costliest)
+        best_holds = []
+        for contract in contracts:
+            if contract.best_no.quantity:
+                best_holds.append(math.floor(share_multiplier * optShares(contract.best_no.pricePerShare)))
+            else:
+                best_holds.append(0)
+        return best_holds
+
+    def optimize_spread(self, max_shares, minimize=True):
+        maximum = max_shares
+        max_profit = 0
+        max_spread = []
+        while maximum > 0:
+            spread = self._best_amount(maximum)
+            maximum -= 1
+            if not minimize:
+                max_spread = spread
+                max_profit = self._potential_profit(spread)
+                break
+            elif max(spread) <= max_shares:
+                if self._potential_profit(spread) > max_profit:
+                    max_spread = spread
+                    max_profit = self._potential_profit(spread)
+        return max_spread, max_profit
+
+    def buy_spread(self, spread):
+        pass
+
+
+class PredictIt:
     def __init__(self, username, password):
         # login_info = {'email': username, 'password': password, 'grant_type': 'password',
         #               'rememberMe': 'false'}
@@ -83,8 +173,29 @@ class API:
         for market in self._markets['markets']:
             yield Market(market, self._orderbook)
 
+    def _get_market_id(self, market_str):
+        guess = re.sub(r'[^\w\s]', '', market_str).lower()
+        guess_words = guess.split()
+        most_matches = 0
+        best_diff = 0
+        best_diff_id = 0
+        for market in self.get_markets():
+            short_name = re.sub(r'[^\w\s]', '', market.short_name).lower()
+            long_name = re.sub(r'[^\w\s]', '', market.name).lower()
+            matches = sum([word in short_name or word in long_name for word in guess_words])
+            diff1 = fuzz.token_sort_ratio(guess, short_name)
+            diff2 = fuzz.token_sort_ratio(guess, long_name)
+            if matches > most_matches or (matches >= most_matches and (diff1 > best_diff or diff2 > best_diff)):
+                best_diff = max(diff1, diff2)
+                best_diff_id = market.id
+            if matches > most_matches:
+                most_matches = matches
+        return best_diff_id
+
     def get_market(self, market_id=None, market_str=None):
         assert market_id or market_str
+        if market_str:
+            market_id = self._get_market_id(market_str)
         markets = self.get_markets()
         if market_id:
             for market in markets:
@@ -92,4 +203,33 @@ class API:
                     return market
 
 
-api = API(auths.username, auths.password)
+class Discord:
+    def __init__(self, pi_api):
+        self.pi_api = pi_api
+
+    def orderbook(self):
+        pass
+
+    def risk(self):
+        pass
+
+    def bins(self):
+        pass
+
+    def related_markets_bin(self):
+        pass
+
+    def related_markets_title(self):
+        pass
+
+    def divide_bins(self):
+        pass
+
+    def value_buy(self):
+        pass
+
+
+api = PredictIt(auths.username, auths.password)
+print(api.get_market(market_str='Dem Nom').name)
+
+
---
+++ utils.py
@@ -0,0 +1,14 @@
+def convert(lst: list) -> str:
+    """
+    convert list to string
+    :param lst: list
+    :return: string
+    """
+    string = ""
+    for n in lst:
+        string += str(n)
+    return string
+
+
+def optShares(cost: float) -> float:
+    return 1.0 / (1.0 - (1.0 - float(cost)) * 0.1)
